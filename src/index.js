66f2da51cf40e0424d72b75209a27331738aa718
anand-yadav55 <anand55yadav@gmail.com>
2021-06-09 08:50:11 +0530

    winning logic

--- src/index.js
+++ src/index.js
@@ -1,112 +1,132 @@
-window.addEventListener("DOMContentLoaded", () => {
-  reset();
-  _Setup();
-});
+let i, j;
+let player = {
+  active: true,
+};
+let board = [];
 
-let _GAMESTATE = null;
-let humanColor = null;
-let computerColor = null;
-let board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-let chances = 0;
-let human = "P";
-let computer = "C";
-let movesAllowed = false;
+// starter
+function Setup() {
+  // creating board
+  const app = document.getElementById("app");
+  let k = 0;
+  for (i = 0; i < 3; i++) {
+    let row = document.createElement("div");
+    for (j = 0; j < 3; j++) {
+      let box = document.createElement("div");
+      box.setAttribute("class", "box");
+      box.setAttribute("id", k++);
+      box.addEventListener("click", move);
+      row.appendChild(box);
+    }
+    app.appendChild(row);
+  }
 
-function _Setup() {
-  checkMovesAllowed();
-  const white = document.getElementById("white");
-  const black = document.getElementById("black");
-
-  white.addEventListener("click", function () {
-    humanColor = "white";
-    computerColor = "black";
-    console.log(humanColor);
-    _Initialize();
-  });
-  black.addEventListener("click", function () {
-    humanColor = "black";
-    computerColor = "white";
-    console.log(humanColor);
-    _Initialize();
-  });
+  //assign each box as blank
+  for (i = 0; i < 9; i++) {
+    board[i] = "B";
+  }
 }
-
-function _Initialize() {
-  movesAllowed = true;
-  const options = document.getElementsByClassName("options");
-  options[0].style.visibility = "hidden";
-  const table = document.getElementsByTagName("table");
-  table[0].style.visibility = "visible";
-  const block = document.getElementsByTagName("td");
-  if (movesAllowed) {
-    for (let i = 0; i < 9; i++) {
-      block[i].addEventListener("click", () => {
-        move(block[i], human, humanColor);
-      });
+//Display Board
+function displayBoard() {
+  let box = document.getElementsByClassName("box");
+  for (i = 0; i < 9; i++) {
+    if (board[i] != "B") {
+      box[i].classList.add("filled");
+      if (board[i] == "C") {
+        box[i].classList.add("C");
+      }
+      if (board[i] == "P1") {
+        box[i].classList.add("P1");
+      }
     }
   }
 }
-function checkMovesAllowed() {
-  if (!movesAllowed) {
-    console.log("not allowed");
-    const block = document.getElementsByTagName("td");
-    for (let i = 0; i < 9; i++) {
-      block[i].removeEventListener("click", () => {
-        move(block[i], human, humanColor);
-      });
+// check moves allowed
+function move(e) {
+  if (player.active) {
+    if (e.target.id >= 0 && e.target.id < 9 && positionAvailable(e)) {
+      board[e.target.id] = "P1";
+      // console.log("moved");
+      player.active = false;
+      displayBoard();
     }
   }
-}
-function move(ele, player, color) {
-  if (board[ele.id] != "P" && board[ele.id] != "C") {
-    ++chances;
-    ele.style.backgroundColor = color;
-    board[ele.id] = player;
-    console.log(board, player);
-    if (checkWinning(player)) {
-      movesAllowed = false;
-      checkMovesAllowed();
-      console.log("WINNER");
-      setTimeout(() => reset(), 500);
-    } else if (chances > 8) {
-      console.log("TIE");
-      setTimeout(() => reset(), 500);
-    } else {
-      round++;
-    }
+  // window.setTimeout(() => {
+  if (!player.active) {
+    aiMove();
+    player.active = true;
+    displayBoard();
   }
+  // }, 500);
+  console.log(board);
+  checkWinner();
 }
-function reset() {
-  humanColor = null;
-  computerColor = null;
-  board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-  chances = 0;
-  human = "P";
-  computer = "C";
-  const options = document.getElementsByClassName("options");
-  options[0].style.visibility = "visible";
-  const table = document.getElementsByTagName("table");
-  table[0].style.visibility = "hidden";
-  const block = document.getElementsByTagName("td");
-  for (let i = 0; i < 9; i++) {
-    block[i].style.backgroundColor = "transparent";
-  }
+function positionAvailable(e) {
+  return !e.target.classList.contains("filled");
+}
 
-  _Setup();
+function aiMove() {
+  let availableMoves = availableMovesForP2();
+  if (availableMoves.length == 0) {
+    // console.log("no moves");
+    return;
+  }
+  let move = availableMoves[Math.floor(Math.random() * availableMoves.length)];
+  // console.log("available" + availableMoves + " computer:" + move);
+  board[move] = "C";
 }
-function checkWinning(player) {
-  if (
-    (board[0] == player && board[1] == player && board[2] == player) ||
-    (board[3] == player && board[4] == player && board[5] == player) ||
-    (board[6] == player && board[7] == player && board[8] == player) ||
-    (board[0] == player && board[3] == player && board[6] == player) ||
-    (board[1] == player && board[4] == player && board[7] == player) ||
-    (board[2] == player && board[5] == player && board[8] == player) ||
-    (board[0] == player && board[4] == player && board[8] == player) ||
-    (board[2] == player && board[4] == player && board[6] == player)
-  ) {
-    return true;
-  } else {
-    return false;
+
+function availableMovesForP2() {
+  let arr = [];
+  for (i = 0; i < 9; i++) {
+    if (board[i] == "B") {
+      arr.push(i);
+    }
+  }
+  return arr;
+}
+
+// winning conditions
+function checkWinner() {
+  const conditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  //All equal check
+  const allEqual = (arr) => arr.every((val) => val === arr[0]);
+
+  for (let i of conditions) {
+    let countP = 0,
+      countC = 0;
+    // console.log("i: " + i);
+    for (let j of i) {
+      // console.log("j: " + j);
+      if (board[j] == "P1") countP++;
+      else if (board[j] == "C") countC++;
+      // console.log("countP: ", countP, "countC: ", countC);
+    }
+    if (countP == 3) {
+      console.log("P1 won");
+      return;
+    } else if (countC == 3) {
+      console.log("C won");
+      return;
+    }
+    // if (allEqual(i)) {
+    //   if (board[] == "P1") {
+    //     console.log("winner P1");
+    //   }
+    //   if ( == "C") {
+    //     console.log("winner C");
+    //   }
+    // }
   }
 }
+
+Setup();
